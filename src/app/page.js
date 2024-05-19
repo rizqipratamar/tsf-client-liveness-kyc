475ff4c24a0af3942226c4faece8945b5966aefe
rizqipratama <rizqi.pratama@ajaib.co.id>
2024-05-19 12:05:43 +0700

    test fix object style

--- src/app/page.js
+++ src/app/page.js
@@ -320,13 +320,18 @@ export default function Home() {
           </div>
         )}
 
-        <video ref={videoRef} autoPlay playsInline className="w-full h-dvh" />
+        <video
+          ref={videoRef}
+          autoPlay
+          playsInline
+          className="w-full h-dvh object-contain "
+        />
         <canvas
           ref={canvasRef}
           width={640}
           height={480}
           style={{ position: "absolute", top: 0 }}
-          className="absolute top-0 w-screen h-dvh object-contain "
+          className="absolute top-0 w-full h-dvh object-contain "
         />
       </main>
     </div>
